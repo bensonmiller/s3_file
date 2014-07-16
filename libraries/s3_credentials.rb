509ce2be45c653f5f2424bef817ec18b339cb2a6
Benson Miller <benson.miller@gmail.com>
2014-07-16 12:19:12 -0700

    Rubocop cleanup

--- libraries/s3_credentials.rb
+++ libraries/s3_credentials.rb
@@ -1,9 +1,8 @@
 require 'rest-client'
 require 'json'
 
-
+# This module provides helper methods for authenticating with S3.
 module S3Credentials
-
   # Returns "best" S3 credentials based on presence (or absence) of
   # access key information in the parameters that are passed.
   # Params:
@@ -15,9 +14,9 @@ module S3Credentials
   # credentials from an EC2 Instance Profile. If that fails, exceptions everywhere.
   def self.get_s3_credentials(aws_access_key_id, aws_secret_access_key, token)
     if aws_access_key_id.nil? && aws_secret_access_key.nil? && token.nil?
-      return get_credentials_from_instance_profile()
+      return credentials_from_instance_profile
     else
-      return {:aws_access_key_id => aws_access_key_id, :aws_secret_access_key => aws_secret_access_key, :token => token}
+      return { :aws_access_key_id => aws_access_key_id, :aws_secret_access_key => aws_secret_access_key, :token => token }
     end
   end
 
@@ -25,12 +24,12 @@ module S3Credentials
   # Params: +none+
   #
   # If not on EC2 or no Instance Profiles are found, raises an exception.
-  def self.get_credentials_from_instance_profile()
+  def self.credentials_from_instance_profile
     instance_profile_base_url = 'http://169.254.169.254/latest/meta-data/iam/security-credentials/'
     begin
       instance_profiles = RestClient.get(instance_profile_base_url)
     rescue RestClient::ResourceNotFound, Errno::ETIMEDOUT # we can either 404 on an EC2 instance, or timeout on non-EC2
-      raise ArgumentError.new 'No credentials provided and no instance profile on this machine.'
+      raise ArgumentError, 'No credentials provided and no instance profile on this machine.'
     end
     instance_profile_name = instance_profiles.split.first
     instance_profile = JSON.load(RestClient.get(instance_profile_base_url + instance_profile_name))
@@ -39,6 +38,6 @@ module S3Credentials
     aws_secret_access_key = instance_profile['SecretAccessKey']
     token = instance_profile['Token']
 
-    return {:token => token, :aws_access_key_id => aws_access_key_id, :aws_secret_access_key => aws_secret_access_key}
+    { :token => token, :aws_access_key_id => aws_access_key_id, :aws_secret_access_key => aws_secret_access_key }
   end
 end
--- libraries/s3_file.rb
+++ libraries/s3_file.rb
@@ -12,96 +12,87 @@ module S3FileLib
       :date => date,
       :authorization => authorization
     }
-    if token
-      headers['x-amz-security-token'] = token
-    end
-
-    return headers
+    headers['x-amz-security-token'] = token if token
+    headers
   end
 
-  def self.file_exists?(bucket,remote_path,aws_access_key_id,aws_secret_access_key,token)
+  def self.file_exists?(bucket, remote_path, aws_access_key_id, aws_secret_access_key, token)
     begin
-      remote_md5 = get_md5_from_s3(bucket,remote_path,aws_access_key_id,aws_secret_access_key,token)
+      remote_md5 = get_md5_from_s3(bucket, remote_path, aws_access_key_id, aws_secret_access_key, token)
     rescue RestClient::ResourceNotFound
       # If this branch is triggered, no file was found at the remote location.
       remote_md5 = nil
     end
-    return remote_md5.nil? ? false : true
+    remote_md5.nil? ? false : true
   end
 
-  def self.get_md5_from_s3(bucket,path,aws_access_key_id,aws_secret_access_key,token)
-    return get_digests_from_s3(bucket,path,aws_access_key_id,aws_secret_access_key,token)["md5"]
+  def self.get_md5_from_s3(bucket, path, aws_access_key_id, aws_secret_access_key, token)
+    get_digests_from_s3(bucket, path, aws_access_key_id, aws_secret_access_key, token)['md5']
   end
 
-  def self.get_digests_from_s3(bucket,path,aws_access_key_id,aws_secret_access_key,token)
-    now, auth_string = get_s3_auth("HEAD", bucket,path,aws_access_key_id,aws_secret_access_key,token,nil,nil)
+  def self.get_digests_from_s3(bucket, path, aws_access_key_id, aws_secret_access_key, token)
+    now, auth_string = get_s3_auth('HEAD', bucket, path, aws_access_key_id, aws_secret_access_key, token, nil, nil)
 
     headers = build_headers(now, auth_string, token)
-    response = RestClient.head('https://%s.s3.amazonaws.com%s' % [bucket,path], headers)
+    response = RestClient.head('https://%s.s3.amazonaws.com%s' % [bucket, path], headers)
 
-    etag = response.headers[:etag].gsub('"','')
+    etag = response.headers[:etag].gsub('"', '')
     digest = response.headers[:x_amz_meta_digest]
-    digests = digest.nil? ? {} : Hash[digest.split(",").map {|a| a.split("=")}]
+    digests = digest.nil? ? {} : Hash[digest.split(",").map { |a| a.split("=") }]
 
-    return {"md5" => etag}.merge(digests)
+    {'md5' => etag}.merge(digests)
   end
 
-  def self.get_from_s3(bucket,path,aws_access_key_id,aws_secret_access_key,token)
-    now, auth_string = get_s3_auth("GET", bucket,path,aws_access_key_id,aws_secret_access_key,token,nil,nil)
+  def self.get_from_s3(bucket, path, aws_access_key_id, aws_secret_access_key, token)
+    now, auth_string = get_s3_auth('GET', bucket, path, aws_access_key_id, aws_secret_access_key, token, nil, nil)
 
     headers = build_headers(now, auth_string, token)
-#    response = RestClient.get('https://%s.s3.amazonaws.com%s' % [bucket,path], headers)
-    response = RestClient::Request.execute(:method => :get, :url => 'https://%s.s3.amazonaws.com%s' % [bucket,path], :raw_response => true, :headers => headers)
+    # response = RestClient.get('https://%s.s3.amazonaws.com%s' % [bucket,path], headers)
+    response = RestClient::Request.execute(:method => :get, :url => 'https://%s.s3.amazonaws.com%s' % [bucket, path], :raw_response => true, :headers => headers)
 
-    return response
+    response
   end
 
-  def self.push_to_s3(bucket,remote_path,aws_access_key_id,aws_secret_access_key,token,local_path,content_md5,content_type)
-    now, auth_string = get_s3_auth("PUT", bucket,remote_path,aws_access_key_id,aws_secret_access_key,token,content_md5,content_type)
+  def self.push_to_s3(bucket, remote_path, aws_access_key_id, aws_secret_access_key, token, local_path, content_md5, content_type)
+    now, auth_string = get_s3_auth('PUT', bucket, remote_path, aws_access_key_id, aws_secret_access_key, token, content_md5, content_type)
 
     headers = build_headers(now, auth_string, token)
     headers[:content_type] = content_type
     headers[:content_md5] = content_md5
 
-    url = 'https://%s.s3.amazonaws.com%s' % [bucket,remote_path]
+    url = 'https://%s.s3.amazonaws.com%s' % [bucket, remote_path]
     response = RestClient.put(url, ::File.read(local_path), headers)
-
-    return response
+    response
   end
 
-  def self.get_s3_auth(method, bucket,path,aws_access_key_id,aws_secret_access_key,token,content_md5,content_type)
-    now = Time.now().utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
+  def self.get_s3_auth(method, bucket, path, aws_access_key_id, aws_secret_access_key, token, content_md5, content_type)
+    now = Time.now.utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
     string_to_sign = "#{method}\n%s\n%s\n%s\n" % [content_md5, content_type, now]
 
-    if token
-      string_to_sign += "x-amz-security-token:#{token}\n"
-    end
+    string_to_sign += "x-amz-security-token:#{token}\n" if token
+    string_to_sign += "/%s%s" % [bucket, path]
 
-    string_to_sign += "/%s%s" % [bucket,path]
-
-    digest = digest = OpenSSL::Digest::Digest.new('sha1')
+    digest = OpenSSL::Digest::Digest.new('sha1')
     signed = OpenSSL::HMAC.digest(digest, aws_secret_access_key, string_to_sign)
     signed_base64 = Base64.encode64(signed)
 
-    auth_string = 'AWS %s:%s' % [aws_access_key_id,signed_base64]
+    auth_string = 'AWS %s:%s' % [aws_access_key_id, signed_base64]
 
-    [now,auth_string]
+    [now, auth_string]
   end
 
   def self.aes256_decrypt(key, file)
-    Chef::Log.debug("Decrypting S3 file.")
+    Chef::Log.debug('Decrypting S3 file.')
     key = key.strip
-    require "digest"
-    key = Digest::SHA256.digest(key) if(key.kind_of?(String) && 32 != key.bytesize)
+    require 'digest'
+    key = Digest::SHA256.digest(key) if key.kind_of?(String) && 32 != key.bytesize
     aes = OpenSSL::Cipher.new('AES-256-CBC')
     aes.decrypt
     aes.key = key
-    decrypt_file = Tempfile.new("chef-s3-decrypt")
-    File.open(decrypt_file, "wb") do |df|
-      File.open(file, "rb") do |fi|
-        while buffer = fi.read(BLOCKSIZE_TO_READ)
-          df.write aes.update(buffer)
-        end
+    decrypt_file = Tempfile.new('chef-s3-decrypt')
+    File.open(decrypt_file, 'wb') do |df|
+      File.open(file, 'rb') do |fi|
+        df.write aes.update(buffer) while buffer == fi.read(BLOCKSIZE_TO_READ)
       end
       df.write aes.final
     end
@@ -112,10 +103,8 @@ module S3FileLib
     recipe_sha256 = checksum
     local_sha256 = Digest::SHA256.new
 
-    File.open(file, "rb") do |fi|
-      while buffer = fi.read(BLOCKSIZE_TO_READ)
-        local_sha256.update buffer
-      end
+    File.open(file, 'rb') do |fi|
+      local_sha256.update buffer while buffer == fi.read(BLOCKSIZE_TO_READ)
     end
 
     Chef::Log.debug "sha256 provided #{recipe_sha256}"
@@ -129,10 +118,8 @@ module S3FileLib
     local_md5 = Digest::MD5.new
 
     # buffer the checksum which should save RAM consumption
-    File.open(file, "rb") do |fi|
-      while buffer = fi.read(BLOCKSIZE_TO_READ)
-        local_md5.update buffer
-      end
+    File.open(file, 'rb') do |fi|
+      local_md5.update buffer while buffer == fi.read(BLOCKSIZE_TO_READ)
     end
 
     Chef::Log.debug "md5 of remote object is #{s3_md5}"
